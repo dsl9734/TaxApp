fa486f73f588e211a9a9f8e50a2d164fd9118f76
diego.sanchez.lizuain@alumnos.upm.es <diego.sanchez.lizuain@alumnos.upm.es>
2021-11-10 19:05:32 +0100

    Cambios en el Mapa, no consigo obtener las coordenadas.

--- TaxApp/Interfaz/App_Usuario/usuario.xaml.cs
+++ TaxApp/Interfaz/App_Usuario/usuario.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DevExpress.Map;
+using DevExpress.Xpf.Map;
 
 namespace TaxApp.Interfaz.App_Usuario
 {
@@ -19,9 +21,38 @@ namespace TaxApp.Interfaz.App_Usuario
     /// </summary>
     public partial class usuario : Window
     {
+        private CoordPoint origen;
+        private CoordPoint destino;
+
         public usuario()
         {
             InitializeComponent();
+        }
+
+        private void Posición(object sender, MouseButtonEventArgs e)
+        {
+            origen = Mapa.ScreenPointToMapUnit(e.GetPosition(""));
+            Origen.Text = origen.ToString();
+        }
+
+        private void Destino(object sender, MouseButtonEventArgs e)
+        {
+            destino = Mapa.ScreenPointToMapUnit(e.GetPosition(""));
+            Destino_T.Text = destino.ToString();
+        }
+
+        private String calcularTarifa(Func<CoordPoint,MapUnit> origen,Func<CoordPoint, MapUnit> destino)
+        {
+            int precio = 0;
+            if(precio >= 0)
+            {
+                return "" + precio;
+            }
+            else
+            {
+                MessageBox.Show("Error: No se ha podido calcular correctamente la tarifa.");
+                return null;
+            }
         }
     }
 }
